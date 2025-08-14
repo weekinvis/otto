e3be472549e5bd6f1dea70140b112bca8ac32270
weekinvis <89652278+weekinvis@users.noreply.github.com>
2025-08-14 09:26:57 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,30 +6,35 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc != 2) {
+    if(argc != 2)
+    {
         imprime_erro("Uso: ./otto [arquivo].blm.");
         return ERRO_QUANTIDADE_ARGV;
     }
 
-    if(!verifica_extensao(argv[1])) {
+    if(!verifica_extensao(argv[1]))
+    {
         imprime_erro("Arquivo com extensao invalida.");
         return ERRO_EXTENSAO_ARQUIVO;
     }
     
     std::ifstream arquivo_blm(argv[1]);
 
-    if(!arquivo_blm) {
+    if(!arquivo_blm)
+    {
         imprime_erro("Arquivo nao encontrado.");
         return ERRO_ARQUIVO_INALCANCAVEL;
     }
 
     std::string linha;
-    while(std::getline(arquivo_blm, linha)) {
+    while(std::getline(arquivo_blm, linha))
+    {
         erro_t erro;
 
         erro = processa_linha(truncador(linha));
 
-        if(erro) {
+        if(erro)
+        {
             return erro;
         }
     }
